cfd43867782c9985af6b5c4cc4141a449ca8353d
manuelps <manuel00silva@gmail.com>
2020-12-01 18:49:58 +0000

    ported reaction handler

---
+++ src/events/message/messageReactionRemove.js
@@ -0,0 +1,81 @@
+const Event = require('../../structures/event');
+
+module.exports = class extends Event {
+
+    async run(messageReaction, user) {
+        if (messageReaction.message.author.id == 511614459672920079) {
+            if (messageReaction.message.channel.id == 612345060008132620) {
+                if (messageReaction.partial) await messageReaction.fetch();
+                if (user.id != 511614459672920079) {
+                    var member = messageReaction.message.guild.members.cache.get(user.id);
+                    //gta
+                    if (messageReaction.emoji.id == 612349938214502495) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "GTA")
+                        );
+                    }
+                    //cs
+                    if (messageReaction.emoji.id == 612351414395469824) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "CS")
+                        );
+                    }
+                    //lol
+                    if (messageReaction.emoji.id == 612351828473937951) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "LoL")
+                        );
+                    }
+                    //Anime
+                    if (messageReaction.emoji.id == 721816608108183583) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "Weeb")
+                        );
+                    }
+                    //MC
+                    if (messageReaction.emoji.id == 722166993779949789) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "Minecraft")
+                        );
+                    }
+                    //Amongus
+                    if (messageReaction.emoji.id == 754373354982735872) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "Among Us")
+                        );
+                    }
+                    //F1
+                    if (messageReaction.emoji.id == 771060089498501141) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "F1")
+                        );
+                    }
+                    //UFC
+                    if (messageReaction.emoji.id == 771060089367953408) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "UFC")
+                        );
+                    }
+                    //Apex
+                    if (messageReaction.emoji.id == 771062410923868221) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "Apex")
+                        );
+                    }
+                    //Genshin
+                    if (messageReaction.emoji.id == 771060542918885436) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "Genshin")
+                        );
+                    }
+                    //Cyberpunk
+                    if (messageReaction.emoji.id == 771060089556697128) {
+                        member.roles.remove(
+                            member.guild.roles.cache.find(role => role.name === "Cyberpunk")
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
--- src/events/ready.js
+++ src/events/ready.js
@@ -8,13 +8,42 @@ module.exports = class extends Event {
         });
     }
 
-    run() {
-        console.log([`Loggado como ${this.client.user.username}!`, 
-            `Carregados ${this.client.commands.size} comandos!`,
-            `Carregados ${this.client.events.size} eventos!`
-            ].join('\n'));
+    async run() {
+        console.log([`Loggado como ${this.client.user.username}!`,
+        `Carregados ${this.client.commands.size} comandos!`,
+        `Carregados ${this.client.events.size} eventos!`
+        ].join('\n'));
 
         this.client.user.setActivity('@notmanzl', { type: 'WATCHING' });
+
+        this.client.guilds.cache
+            .get("337617578128048129")
+            .rulesChannel
+            .messages.fetch("612960847160934400")
+            .then(function (message) {
+                //gta
+                message.react(message.guild.emojis.cache.get("612349938214502495"));
+                //cs
+                message.react(message.guild.emojis.cache.get("612351414395469824"));
+                //lol
+                message.react(message.guild.emojis.cache.get("612351828473937951"));
+                //Anime
+                message.react(message.guild.emojis.cache.get("721816608108183583"));
+                //MC
+                message.react(message.guild.emojis.cache.get("722166993779949789"));
+                //amongus
+                message.react(message.guild.emojis.cache.get("754373354982735872"));
+                //f1    
+                message.react(message.guild.emojis.cache.get("771060089498501141"));
+                //ufc
+                message.react(message.guild.emojis.cache.get("771060089367953408"));
+                //apex
+                message.react(message.guild.emojis.cache.get("771062410923868221"));
+                //genshin
+                message.react(message.guild.emojis.cache.get("771060542918885436"));
+                //cyberpunk
+                message.react(message.guild.emojis.cache.get("771060089556697128"));
+            });
 
     }
 
