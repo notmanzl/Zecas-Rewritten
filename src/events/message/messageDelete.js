0762f2407ba5d49b55c5a5a815915f6f19345c3e
manuelps <manuel00silva@gmail.com>
2020-12-01 16:05:18 +0000

    did message deletion logging

---
+++ src/events/message/messageDelete.js
@@ -0,0 +1,27 @@
+const Event = require('../../structures/event');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Event {
+
+    async run(message) {
+        if (message.author.bot) return;
+        const attachments = message.attachments.size ? message.attachments.map(attachment => attachment.proxyURL) : null;
+        const embed = new MessageEmbed()
+            .setColor(message.member.displayColor)
+            .setAuthor(message.author.tag, this.client.user.displayAvatarURL({ dynamic: true, size: 4096 }))
+            .setTitle('Mensagem Apagada')
+            .setDescription([
+                `**• ID:** ${message.id}`,
+                `**• Channel:** ${message.channel}`,
+                `**• Autor:** ${message.member.displayName}`,
+                `${attachments ? `**• Ficheiros:** ${attachments.join('\n')}` : ''}`
+            ]);
+        if (message.content.length) {
+            embed.addField(`**• Mensagem:**`, `${message.content}`);
+        }
+
+        const channel = message.guild.channels.cache.find(ch => ch.name === 'deletedlogs');
+        if (channel) channel.send(embed);
+    }
+
+}
