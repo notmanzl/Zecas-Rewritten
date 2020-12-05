620a88fff9c65f78730526096a015259b7f723ae
manuelps <manuel00silva@gmail.com>
2020-12-05 16:16:03 +0000

    added timestamp on message delete

--- src/events/message/messageDelete.js
+++ src/events/message/messageDelete.js
@@ -11,6 +11,7 @@ module.exports = class extends Event {
             .setAuthor(message.author.tag, this.client.user.displayAvatarURL({ dynamic: true, size: 4096 }))
             .setTitle('Mensagem Apagada')
             .setThumbnail(message.member.user.displayAvatarURL({ dynamic: true, size: 4096 }))
+            .setTimestamp()
             .setDescription([
                 `**• ID:** ${message.id}`,
                 `**• Channel:** ${message.channel}`,
