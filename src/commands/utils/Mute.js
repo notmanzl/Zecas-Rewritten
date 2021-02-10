1de3323785ef62adc659adbdb9753df9fdd73a8e
notmanzl <manuel00silva@gmail.com>
2021-02-10 00:16:00 +0000

    added voice mute

--- src/commands/utils/Mute.js
+++ src/commands/utils/Mute.js
@@ -17,12 +17,14 @@ module.exports = class extends Command {
         const member = message.mentions.members.last() || message.guild.members.cache.get(target) || message.member;
         let muterole = message.guild.roles.cache.find(role => role.name === "Muted");
         if (message.member.hasPermission("MUTE_MEMBERS")) {
-            if(member.roles.cache.has(muterole.id)) {
+            if(member.roles.cache.has(muterole.id) || member.voice.serverMute) {
                 member.roles.remove(muterole);
-                return message.channel.send(`${member} foi unmuted.`);
+                member.setMute(false);
+                return message.channel.send(`${member} foi **unmuted**.`);
             } else {
                 member.roles.add(muterole);
-                return message.channel.send(`${member} foi muted.`);
+                member.setMute(true);
+                return message.channel.send(`${member} foi **muted**.`);
             }
         } else return message.channel.send("Não tens permissão para isto.");
     }
